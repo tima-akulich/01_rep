cb042be97402799b833f21c71abd5bdbeaae2714
Roman Mikulich <r.mikulich@mail.ru>
2018-09-07 21:53:53 +0300

    first project

---
+++ loops.py
@@ -0,0 +1,26 @@
+i = 10
+num = int(input("Num: "))
+while i>0:
+    print(i)
+    i -=1
+
+    if i == num:
+        print("Break")
+        break #перывает while
+else:
+    print("Else")
+
+
+print(--------------)
+
+try:
+    1/0 #(исключение, условие)
+except ZeroDivisionError: #название ичключения,
+                          # можно несколько в ()
+    print("Exception")
+else:
+    print("Try else") #если вообще нет ошибок
+finally:
+
+
+# ключем в дикте могут быть только неизменяемые
